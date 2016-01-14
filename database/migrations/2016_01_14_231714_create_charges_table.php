551af5532f95f83cb8e753b3fd2e0f0defef2e3c
Jyrone Parker <jyrone.parker@gmail.com>
2016-01-14 23:20:50 +0000

    Added skeleton for migrations

---
+++ database/migrations/2016_01_14_231714_create_charges_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChargesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('charges', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('charges');
+    }
+}

fc0a0ba7df97de7195ca852e30d33e39d280257c
Jyrone Parker <jyrone.parker@gmail.com>
2016-01-15 00:15:51 +0000

    Charges migration

--- database/migrations/2016_01_14_231714_create_charges_table.php
+++ database/migrations/2016_01_14_231714_create_charges_table.php
@@ -14,6 +14,17 @@ class CreateChargesTable extends Migration
     {
         Schema::create('charges', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+            ->references('id')->on('users')
+            ->onDelete('cascade');
+            $table->integer('customer_id')->unsigned();
+            $table->foreign('customer_id')
+            ->references('id')->on('customers')
+            ->onDelete('cascade');
+            $table->integer('amount');
+            $table->string('description');
+            $table->string('receipt_email');
             $table->timestamps();
         });
     }
